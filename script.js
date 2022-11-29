a76769efa5bea0d580af5799b12ca515a2b2f5ce
Daniel Mello <danielmello.nfa@gmail.com>
2022-11-29 16:40:27 -0300

    Landing Page e Pagina de Projetos

--- script.js
+++ script.js
@@ -88,3 +88,47 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+const typedTextSpanS = document.querySelector(".typedTextSwap");
+const cursorSpanS = document.querySelector(".cursorSwap");
+
+const textArrayS = ["Desafiador","Evoluir","Aprender","Legal","Vida"];
+const typingDelayS = 200;
+const erasingDelayS = 50;
+const newTextDelayS = 500; // Delay between current and next text
+let textArraySIndexS = 0;
+let charIndexS = 0;
+
+function type() {
+  if (charIndexS < textArrayS[textArraySIndexS].length) {
+    if(!cursorSpanS.classList.contains("typing")) cursorSpanS.classList.add("typing");
+    typedTextSpanS.textContent += textArrayS[textArraySIndexS].charAt(charIndexS);
+    charIndexS++;
+    setTimeout(type, typingDelayS);
+  } 
+  else {
+    cursorSpanS.classList.remove("typing");
+  	setTimeout(erase, newTextDelayS);
+  }
+}
+
+function erase() {
+	if (charIndexS > 0) {
+    if(!cursorSpanS.classList.contains("typing")) cursorSpanS.classList.add("typing");
+    typedTextSpanS.textContent = textArrayS[textArraySIndexS].substring(0, charIndexS-1);
+    charIndexS--;
+    setTimeout(erase, erasingDelayS);
+  } 
+  else {
+    cursorSpanS.classList.remove("typing");
+    textArraySIndexS++;
+    if(textArraySIndexS>=textArrayS.length) textArraySIndexS=0;
+    setTimeout(type, typingDelayS + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArrayS.length) setTimeout(type, newTextDelayS + 250);
+});
+
+
