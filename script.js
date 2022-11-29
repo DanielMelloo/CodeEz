cef37ee2db7bb6ae41ecb1b0674637e59952900e
Daniel Mello <danielmello.nfa@gmail.com>
2022-11-29 19:52:34 -0300

    Att e Correção de Responsividade

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
+
+
+
+
+
+
+
+
+
 const typedTextSpanS = document.querySelector(".typedTextSwap");
 const cursorSpanS = document.querySelector(".cursorSwap");
 
@@ -50,36 +59,36 @@ const newTextDelayS = 500; // Delay between current and next text
 let textArraySIndexS = 0;
 let charIndexS = 0;
 
-function type() {
+function typeS() {
   if (charIndexS < textArrayS[textArraySIndexS].length) {
     if(!cursorSpanS.classList.contains("typing")) cursorSpanS.classList.add("typing");
     typedTextSpanS.textContent += textArrayS[textArraySIndexS].charAt(charIndexS);
     charIndexS++;
-    setTimeout(type, typingDelayS);
+    setTimeout(typeS, typingDelayS);
   } 
   else {
     cursorSpanS.classList.remove("typing");
-  	setTimeout(erase, newTextDelayS);
+  	setTimeout(eraseS, newTextDelayS);
   }
 }
 
-function erase() {
+function eraseS() {
 	if (charIndexS > 0) {
     if(!cursorSpanS.classList.contains("typing")) cursorSpanS.classList.add("typing");
     typedTextSpanS.textContent = textArrayS[textArraySIndexS].substring(0, charIndexS-1);
     charIndexS--;
-    setTimeout(erase, erasingDelayS);
+    setTimeout(eraseS, erasingDelayS);
   } 
   else {
     cursorSpanS.classList.remove("typing");
     textArraySIndexS++;
     if(textArraySIndexS>=textArrayS.length) textArraySIndexS=0;
-    setTimeout(type, typingDelayS + 1100);
+    setTimeout(typeS, typingDelayS + 1100);
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArrayS.length) setTimeout(type, newTextDelayS + 250);
+  if(textArrayS.length) setTimeout(typeS, newTextDelayS + 250);
 });
 
 
